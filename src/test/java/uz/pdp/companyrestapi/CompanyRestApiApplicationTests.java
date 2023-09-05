8fd21c87288f810e0f1d76b77348f64c0ea573a6
Xojiakbar <87039534+XojiakbarA@users.noreply.github.com>
2023-09-05 21:34:33 +0500

    init

---
+++ src/test/java/uz/pdp/companyrestapi/CompanyRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.companyrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
